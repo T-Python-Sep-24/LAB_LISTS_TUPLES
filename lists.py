8be2c7663bc8406cf750cd96d8a849a9a117a9b5
Mohamed <sirexplq@gmail.com>
2024-10-07 20:21:25 +0300

    correctQ1

--- lists.py
+++ lists.py
@@ -1,12 +1,11 @@
 #q1
 def list_1(number1):
-    for x in (number1):
-        print(x)
-list_2=[1,2,3]
-print("the answer for q1 is : ")
-list_1(list_2)
+    return sum(number1)
+my_list = [2, 3, 4, 5, 15, 1, 43, 20]
+result1 = list_1(my_list)
+print("the answer for q1 is : ", result1)
 #q2
-print("the answer for q2 is : ",max(list_2))
+print("the answer for q2 is : ",max(my_list))
 #q3
 odd_list= [i for i in range(1200,2001,125)  if i%2 !=0 ]
 print("the answer for q3 is : ",odd_list)
